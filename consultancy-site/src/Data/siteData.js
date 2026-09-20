2a8205d722f7b1f25958757e557f81dcd0b1f4d0
Ahsan Khan <ahsan125384@gmail.com>
2026-09-20 18:35:13 +0500

    sdsd

--- consultancy-site/src/Data/siteData.js
+++ consultancy-site/src/Data/siteData.js
@@ -3,8 +3,8 @@ export const navLinks = [
   // { label: "About", href: "#about" },
   { label: "Services", href: "/services" },
  { label: "Destinations", href: "/destinations" },
-  { label: "Opportunities", href: "#opportunities" },
-  { label: "Stories", href: "#SuccessStories" },
+  // { label: "Opportunities", href: "#opportunities" },
+  // { label: "Stories", href: "#SuccessStories" },
   { label: "Process", href: "#process" },
 ];
 
